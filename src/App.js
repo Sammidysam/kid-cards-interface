c3c2b1147d556789c73b6494ce3ff09e54cabc8c
Sam Craig <craigsamm@gmail.com>
2018-10-06 12:15:52 -0400

    Prevent error upon missing doctors

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 	render() {
 		return (
             <div className="KidCards" background-color="#D2B38C">
-                <h1 align="center">{this.state.doctors[0].patients[0].name}</h1>
+                <h1 align="center">{this.state.doctors && this.state.doctors[0].patients[0].name}</h1>
 			<CardStack cards={this.state.doctors && this.state.doctors[0].patients[0].notes} />
 			</div>
 		);
