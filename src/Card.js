1f8af5f067086983a3487e75fb2b36fcaf8497db
sranga12 <35714412+sranga12@users.noreply.github.com>
2018-10-06 11:02:37 -0400

    updated cards

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,32 @@
 import React, { Component } from "react"
+import Moment from 'react-moment'
+import 'moment-timezone'
 
 class Card extends Component {
     constructor(props) {
         super(props);
-        this.state = { liked: false };
+    }
+
+    color() {
+        switch (this.props.creator.role) {
+            case ('Parent' | 'Mom' | 'Dad'):
+                return 'border-dark';
+            case ('Daycare'):
+                return 'border-success';
+            case ('Teacher' | 'Professor'):
+                return 'border-warning';
+            default:
+                return 'border-dark';
+        }
     }
 
     render() {
+        
         return (
-			<div className="card border-dark mb-3">
+			<div className="card {this.color()} mb-3">
 			<div className="card-body">
 			<p className="card-text">{this.props.body}</p>
-			<p className="card-text"><small className="text-muted">{this.props.date}</small></p>
+                    <p className="card-text"><small className="text-muted">{this.props.creator.name}+"("+{this.props.creator.role}+") "+<Moment fromNow>{this.props.date}</Moment> </small></p>
 			</div>
 			</div>
         );
