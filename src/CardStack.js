f2ce49899c4b8589b26201aa8ad303742b9662f3
Sam Craig <craigsamm@gmail.com>
2018-10-06 12:23:51 -0400

    Fix background color

--- src/CardStack.js
+++ src/CardStack.js
@@ -5,7 +5,7 @@ import Card from "./Card"
 class CardStack extends Component {
     render() {
         return (
-			<div id="cardstack" className="card-columns">
+			<div id="cardstack" className="card-columns CardStack">
             {this.props.cards && this.props.cards.map(m => <Card body={m.body} key={m.date} date={m.date} creator={m.creator} />)}
 			</div>
 			);
