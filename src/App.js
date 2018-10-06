6a12494d91b2eabcc4657d62f09f09a59b4b4d46
sranga12 <35714412+sranga12@users.noreply.github.com>
2018-10-06 13:20:00 -0400

    push

--- src/App.js
+++ src/App.js
@@ -6,28 +6,34 @@ import base from "./config"
 import CardStack from "./CardStack"
 
 class App extends Component {
-	constructor () {
-		super();
+    constructor() {
+        super();
 
-		this.state = {};
-	}
+        this.state = {};
+    }
 
-	render() {
-		return (
-            <div className="KidCards" style={{backgroundColor: "#D2B38C"}}>
+    render() {
+        return (
+            <div className="KidCards" style={{ backgroundColor: "#D2B38C" }}>
                 <h1 align="center">{this.state.doctors && this.state.doctors[0].patients[0].name}</h1>
-			<CardStack cards={this.state.doctors && this.state.doctors[0].patients[0].notes} />
-			</div>
-		);
-	}
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Keywords (fever, fidget, sad...)"></input>
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" type="button">Analyze</button>
+                    </div>
+                </div>
+                <CardStack cards={this.state.doctors && this.state.doctors[0].patients[0].notes} />
+            </div>
+        );
+    }
 
-	componentDidMount () {
-		base.bindToState("/doctors", {
-			context: this,
-			asArray: true,
-			state: "doctors"
-		})
-	}
+    componentDidMount() {
+        base.bindToState("/doctors", {
+            context: this,
+            asArray: true,
+            state: "doctors"
+        })
+    }
 }
 
 export default App;
